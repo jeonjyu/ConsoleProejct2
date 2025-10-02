f4a8d9bc5c615d166fcb85f8aad771fdaf97d425
jeonjyu <jihoojeon77@gmail.com>
2025-10-02 21:10:37 +0900

    [Feat] 플레이어 관리 기능 추가
    생성자, 입력 처리, 입력 판단

--- ShapeSort/Player.cs
+++ ShapeSort/Player.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace ShapeSort
 {
-    internal class Player
+    public class Player
     {
+        static int life;
+
+        public int Life { get; set; }
+
+        public Player()
+        {
+            Life = 5;
+        }
+
+        // 
+        public Shape GetPlayerInput()
+        {
+            bool isValid = false;
+            while (!isValid)
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                switch (input.KeyChar)
+                {
+                    case 'W':
+                        return Shape.Etc;
+                    case 'A':
+                        return Shape.Circle;
+                    case 'S':
+                        return Shape.Triangle;
+                    case 'D':
+                        return Shape.Square;
+                    default:
+                        continue;
+                }
+                isValid = true;
+            }
+            return Shape.Etc;
+        }
+
+        // 플레이어가 분류한 도형이 맞는지 판단하는 함수
+        public void SortShape(Shape inShape, Shape desBasket)
+        {
+            if (desBasket == Shape.Etc && inShape != desBasket)
+            {
+                Life--;
+            }
+        }
+        
     }
 }
