acd3a54ff598117e6ecb649a823b0b55f8a8f784
jeonjyu <jihoojeon77@gmail.com>
2025-10-02 21:09:40 +0900

    [Feat] 대기열 관리 기능 추가
    enqueue, dequeue, peek, count 메서드

---
+++ ShapeSort/ShapeQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShapeSort
+{
+    public class ShapeQueue
+    {
+        private Queue<Shape> _shapesQueue = new Queue<Shape>(30);
+
+        public int Counts()
+        {
+            return _shapesQueue.Count;
+        }
+
+        public Shape CurrentShape()
+        {
+            return _shapesQueue.Peek();
+        }
+
+        public Queue<Shape> EqShapes()
+        {
+            Random random = new Random();
+            for (int i = 0; i < _shapesQueue.Count; i++)
+            {
+                _shapesQueue.Enqueue((Shape)random.Next(0, 4));
+            }
+
+            return _shapesQueue;
+        }
+
+        public Shape DqShapes()
+        {
+            return _shapesQueue.Dequeue();
+        }
+    }
+}
